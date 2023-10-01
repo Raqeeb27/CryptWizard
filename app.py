443d9750d22262627b22959b063cc3451e23eb51
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2023-10-01 13:58:22 +0530

    updated, DB to store

--- app.py
+++ app.py
@@ -1,10 +1,71 @@
 ## Import Modules
+import mysql.connector
+from mysql.connector import Error
 import os
 from cryptography.fernet import Fernet
 import getpass
 from time import time, sleep
+import bcrypt
 
 ##------------------------------------------------------------------------
+# Function to establish a database connection
+
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'asdf'
+}
+
+def connect_to_database():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print("\nError connecting to the database:", e)
+    return None
+
+def initialize_database():
+    connection = connect_to_database()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            
+            # Create the database if it doesn't exist
+            cursor.execute("CREATE DATABASE IF NOT EXISTS passwordmanager")
+            cursor.execute("USE passwordmanager")
+
+            # Create the Users table if it doesn't exist
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Users (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    username VARCHAR(255) NOT NULL,
+                    salt CHAR(29) NOT NULL,
+                    master_key VARCHAR(255) NOT NULL
+                )
+            """)
+
+            # Create the Passwords table if it doesn't exist
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Passwords (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    user_id INT,
+                    website VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES Users(id)
+                )
+            """)
+
+            connection.commit()
+            cursor.close()
+            connection.close()
+            db_config['database'] = 'passwordmanager'
+        except Error as e:
+            print("Error initializing database:", e)
+
+initialize_database()
+
+'''##------------------------------------------------------------------------
 # Key Generation Function
 
 def generate_key():
@@ -27,52 +88,75 @@ def load_key():
     except:
         print("\nError in Loading the Key !!!\n")
         exit()
-
+'''
 ##------------------------------------------------------------------------
-# User class
+# 
 
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.passwords = {} # Dictionary to store passwords
+def add_password(user_id, website, password):
+    connection = connect_to_database()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            query = "INSERT INTO Passwords (user_id, website, password) VALUES (%s, %s, %s)"
+            cursor.execute(query, (user_id, website, password))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print(f"\nPassword for '{website}' saved successfully!")
+            input("\nPress Enter to Continue....")
+        except Error as e:
+            print("Error adding password:", e)
+    
+# Function to retrieve a password for a user from the database
+def get_password(user_id, username, website):
+    connection = connect_to_database()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            query = "SELECT password FROM Passwords WHERE user_id = %s AND website = %s"
+            cursor.execute(query, (user_id, website))
+            result = cursor.fetchone()
+            cursor.close()
+            connection.close()
+            if result:
+                print(f"\nUsername: {username}")
+                print(f"Password for '{website}' : {result[0]}")
+            else:
+                print(f"\nNo password found for '{website}'")
+            input("\nPress Enter to Continue....")
+        except Error as e:
+            print("Error retrieving password:", e)
+'''
+    if website in passwords:
+        password = passwords[website]
+        print(f"Username: {username}")
+        print(f"Password for {website} : {password}")
+    else:
+        print(f"\nNo password found for {website}")
+    input("\nPress Enter to Continue....")
+    '''
 
-    def add_password(self, website, password):
-        self.passwords[website] = password
-        print(f"\nPassword for {website} saved successfully!")
-        input("\nPress Enter to Continue....")
-
-    def get_password(self, website):
-        if website in self.passwords:
-            password = self.passwords[website]
-            print(f"Username: {self.username}")
-            print(f"Password for {website} : {password}")
-        else:
-            print(f"\nNo password found for {website}")
-        input("\nPress Enter to Continue....")
-
-# Create a list to store User objects
-users = []
 
 ##------------------------------------------------------------------------
 # Function to check unername and password format
 def is_valid(cred_type,visible = True):
     if cred_type == ("Username") :
-        credential = input("\nEnter Username: ").title().strip()
+        credential = input("\nEnter Username : ").title().strip()
         # Checking username length
         if credential == '' :
             print('Username required !!!')
             return None
-        elif len(credential) < 3 or len(credential) > 15 :
-            print("Only 3 - 15 characters allowed in Username")
+        elif len(credential) < 3 or len(credential) > 25 :
+            print("Only 3 - 25 characters allowed in Username")
             return None
         else:
             return credential
+        
     elif cred_type == ("Password"):
         if visible:
-            credential = input("Enter Password: ")
+            credential = input("Enter Password : ")
         else:
-            credential = getpass.getpass("Enter Password: ")
+            credential = getpass.getpass("Enter Password : ")
         # Checking password length
         if credential == '':
             print('User Password required !!!')
@@ -80,15 +164,24 @@ def is_valid(cred_type,visible = True):
         elif credential.count(" ") != 0:
             print("\nPassword can't have space!!!")
             return None
-        elif len(credential) < 4 or len(credential) > 12:
-            print("\nPassword must have 4 - 12 characters !!!")
+        elif len(credential) < 4 or len(credential) > 25:
+            print("\nPassword must have 4 - 25 characters !!!")
             return None
         else:
             return credential
 
-    else:
+    elif cred_type == ("Website") :
+        credential = input("\nEnter Website : ").strip()
         # Checking website length
-        pass
+        if credential == '' :
+            print('Website required !!!')
+            return None
+        else:
+            return credential
+
+def hash_master_key(master_password, salt):
+    return bcrypt.hashpw(master_password.encode('utf-8'), salt)
+
 
 ##------------------------------------------------------------------------
 # Create User Function
@@ -102,14 +195,30 @@ def create_user():
         return
     cred_check = is_valid("Password")
     if cred_check:
-        password = cred_check
+        master_password = cred_check
     else:
         return 
     confirm_password = input("Confirm Password: ")
-    if confirm_password == password :
-        user = User(username, password)
-        users.append(user)
-        print("\nUser created successfully!")
+    if confirm_password == master_password :
+        
+        # Generate a random salt
+        salt = bcrypt.gensalt()
+        
+        # Derive the master key using the master password and salt
+        hashed_master_key = hash_master_key(master_password, salt)
+
+        connection = connect_to_database()
+        if connection:
+            try:
+                cursor = connection.cursor()
+                query = "INSERT INTO Users (username, salt, master_key) VALUES (%s, %s, %s)"
+                cursor.execute(query, (username, salt, hashed_master_key))
+                connection.commit()
+                cursor.close()
+                connection.close()
+                print("\nUser created successfully!")
+            except Error as e:
+                print(f"Error creating User Account: {e}\n")
     else:
         print("\nPassword didn't match,\nUser account creation failed!")
 
@@ -117,28 +226,66 @@ def create_user():
 # User Login Function 
 
 def user_login():
-    print("\n---- LOGIN USER ----")
+    print("\n---- USER LOGIN ----")
     cred_check = is_valid("Username")
     if cred_check:
         username = cred_check
     else:
-        return
+        return None,None
     cred_check = is_valid("Password",False)
     if cred_check:
-        password = cred_check
+        input_password = cred_check
     else:
-        return
-    for user in users:
-        if user.username == username and user.password == password:
-            print("\nLogin Successfull !!!\n")
-            sleep(0.8)
-            return user,username
-        elif user.username == username and user.password != password:
-            print("Login failed !!! Invalid credentials.")
-            return None
-    print("\nUser doesn't exist\nPlease create your User account")
-    return None
+        return None,None
+    connection = connect_to_database()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            query1 = "SELECT id, username, salt, master_key FROM Users WHERE username = %s"
+            cursor.execute(query1, (username, ))
+            result = cursor.fetchone()
+            if result:
+                logged_user_id, logged_username, stored_salt, stored_master_key = result
+                #2
+                # print("logged_user_id = "+logged_user_id)
+                print("logged_username = "+logged_username)
+                print("stored_salt = "+stored_salt)
+                print("stored_master_key = "+stored_master_key)
 
+                # Derive the master key using the provided master password and stored salt
+                input_master_key = hash_master_key(input_password, stored_salt)
+                print("input_master_key = "+input_master_key)
+
+                if input_master_key == stored_master_key:
+                    print("Authentication successful !!!")
+                    # Use input_master_key to decrypt user data
+                    sleep(1)
+                    return logged_user_id,logged_username
+                else:
+                    print("Authentication failed !!!\nUsername or Password is Incorrect\n")
+            else:
+                print("Unrecognized User. Authentication failed !!!")
+                '''print("\nLogin Successfull !!!")
+                sleep(1)
+                return result_id[0],username
+            cursor = connection.cursor()
+            query2 = "SELECT id FROM Users WHERE username = %s"
+            cursor.execute(query2, (username,))
+            result_user = cursor.fetchone()       
+            if result_user:
+                print("\nLogin failed !!! Invalid credentials.")
+                return None,None
+            else:
+                print("\nUser doesn't exist\nPlease create your User account")'''
+                return None,None
+            
+        except Error as e:
+            print("Error in User Login: ", e)
+        finally:
+            cursor.close()
+            connection.close()
+
+'''
 ##------------------------------------------------------------------------
 # Access Control
 
@@ -151,11 +298,12 @@ if master_pwd != '2023':
 print("\nACCESS GRANTED !!!\nWelcome\n")
 sleep(1)
 
+
 ##------------------------------------------------------------------------
-## Setting up Password_Manager_Files directory
+## Setting up passwordmanager_Files directory
 
 workingDirectory = os.getcwd()
-manager_dir = 'Password_Manager_Files'
+manager_dir = 'passwordmanager_Files'
 users_dir = 'Users'
 path = os.path.join(workingDirectory, manager_dir)
 users_path = os.path.join(path, users_dir)
@@ -175,11 +323,11 @@ except:
 ##------------------------------------------------------------------------
 # Files
 
-master_key = 'Password_Manager_Files/master_key.key'
+master_key = 'passwordmanager_Files/master_key.key'
 
 ##------------------------------------------------------------------------
 # Initializing password.txt file
-'''
+
 if (not os.path.exists(passwords_file)) or os.path.getsize(passwords_file) == 0:
     try:
         with open(passwords_file, 'a') as file:
@@ -187,7 +335,7 @@ if (not os.path.exists(passwords_file)) or os.path.getsize(passwords_file) == 0:
             ------------------------------------------------------------------------------------" )
     except:
         print("\nError in Creating passwords.txt file !!!\n")
-        exit()'''
+        exit()
     
 ##------------------------------------------------------------------------
 # Generate Key_file
@@ -200,21 +348,26 @@ if not os.path.exists(master_key):
 
 key = load_key() + master_pwd.encode()
 master_key = Fernet(key)
-
+'''
 ##------------------------------------------------------------------------
 # Menu Drien User display
 
 def user_display():
     while True:
-        print(f"\n--------\n| User | --> {user_name}\n--------\n")
-        print("1. Add a new password")
-        print("2. Retrieve a password")
+        print(f"\n--------\n| User | --> {logged_username}\n--------\n")
+        print("1. New Password Record")
+        print("2. Retrieve password")
         print("3. Logout\n")
 
         user_choice = input(" -->  ")
 
         if user_choice == '1':
-            website = input("\nEnter Website : ").strip()
+            cred_check = is_valid("Website")
+            if cred_check:
+                website = cred_check
+            else:
+                return
+            
             cred_check = is_valid("Password")
             if cred_check:
                 password = cred_check
@@ -222,12 +375,16 @@ def user_display():
                 return
             confirm_password = input("Confirm Password: ")
             if confirm_password == password :
-                logged_in_user.add_password(website, password)
+                add_password(logged_user_id,website, password)
             else:
                 print("\nPassword didn't match\nFailed to save Website and Password!")
         elif user_choice == '2':
-            website = input("\nEnter website to retrieve password: ").strip()
-            logged_in_user.get_password(website)
+            cred_check = is_valid("Website")
+            if cred_check:
+                website = cred_check
+            else:
+                return
+            get_password(logged_user_id,logged_username,website)
 
         elif user_choice == '3':
             print("\nLogout Successfull !!!")
@@ -241,14 +398,14 @@ def user_display():
 
 while True:
 
-    choice = input("\n----- PASSWORD MANAGER -----\n\n1. CREATE New User\n2. Login\n3. Exit\n\n -->  ")
+    choice = input("\n----- PASSWORD MANAGER -----\n\n1. Register\n2. Login\n3. Exit\n\n -->  ")
 
     if choice == '1':
         create_user()
         input("\nPress Enter to Continue....")        
     elif choice == '2':
-        logged_in_user,user_name = user_login()
-        if logged_in_user:
+        logged_user_id,logged_username = user_login()
+        if logged_user_id:
             user_display()
             input("\nPress Enter to Continue....")
         else:
