b222f19bc089dce960fe4c7f8ec461b90ae3709b
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2023-10-16 18:57:33 +0530

    Many upates, Initialized Home,Register and signin pages

---
+++ CryptWizard/home/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
+    path('signin/', views.signin, name='signin'),
+]
